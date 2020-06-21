6d5212967ef46a45e632b5a70f2c51c671cc31e4
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-06-21 17:55:04 -0300

    Métodos HTTP

--- GoStack-Bootcamp/conceito-dev/backend/src/index.js
+++ GoStack-Bootcamp/conceito-dev/backend/src/index.js
@@ -3,14 +3,49 @@ const { response } = require('express');
 
 const app = express(); //Declarando uma variável que e igual a express
 
-app.get('/', (request, response) => { //Metodo get que recebe dois parâmetros.
-  //Um endreço, podendo ser '/' e umma função que recebe dois parâmetros resq e resp.
-  //response permite retornar várias resposta ao usuário e request guarda requisição do usuário.
-  return response.json(
-    { menssagen: 'Hello World' } //json deve sempre retornado em array [] ou objeto {}
-  ) 
-  
-})  
+/**
+ * Métodos HTTP
+ * 
+ * - GET: Buscar informações do back-end
+ * - POST: Criar uma informação no back-end
+ * - PUT/PATH: Alterar uma informação no back-end
+ *    - PUT: Utilizado para alterar varias informações
+ *    - PATH: Utilizado para alter informações especificas
+ * - DELETE: Deletar uma informação no back-end
+ */
+
+app.get('/projects', (request, response) => { //Metodo get que recebe dois parâmetros.
+  //Um endreço, podendo ser '/' e o recurso que usuário quer acessar '/projects'.
+  //E uma função que recebe dois parâmetros resq e resp.
+  //response permite retornar resposta ao usuário e request guarda requisição do usuário.
+  return response.json([ //json deve sempre retornado em array [] ou objeto {}
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => { //Para alterar utilizamos um id
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => { //Para deletar utilizamos um id
+  return response.json([
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
 
 app.listen(3333, () => { //adiciona uma porta, podemos escolher qualquer porta acima da porta 80.
   //Como segundo parâmetro, adicionamos um função automática que mostra quando o servidor estiver no ar
