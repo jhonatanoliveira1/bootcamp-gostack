8ea10d046b6003ffeefeabd1ae7ad111e0aeec4d
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-06-24 23:00:07 -0300

    Estado e Imutabilidade

--- GoStack-Bootcamp/conceitos-dev/frontend/src/App.js
+++ GoStack-Bootcamp/conceitos-dev/frontend/src/App.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 /**
  * Componente
  * Propriedade
- * Estado
+ * Estado & Imutabilidade
  */
 
 function App() {
+  const [projects, setProjects ] = useState([ 'Desenvolvimento de app', 'Front-end web']);
+
+  // useStates retorna um array com 2 posições
+  //
+  // 1. Variável com o seu valor inicial
+  // 2. Função para atualizarmos esse valor
+
+  function handleAppProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]); //Alterando o valor indiretamente 
+  }
+
   return (
     <>
-      <Header title="Homepage"> 
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAppProject}>Adicionar projeto</button>
     </>
   );
 }
