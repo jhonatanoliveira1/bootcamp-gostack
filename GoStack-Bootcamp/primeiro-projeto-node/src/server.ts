c866cfe311c148457412774abf9ea3ebcc82db26
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-07-01 20:38:32 -0300

    [Padrões de projeto com ESLint, Prettier e EditorC

--- GoStack-Bootcamp/primeiro-projeto-node/src/server.ts
+++ GoStack-Bootcamp/primeiro-projeto-node/src/server.ts
@@ -1,10 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ menssagem: 'Hello GoStack'});
-});
+app.get('/', (request, response) => response.json({ menssagem: 'Hello GoStack' }));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
