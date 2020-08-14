39fe8218d2af96308bf2f2c8b0cc726599d543ab
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-08-14 14:26:48 -0300

    GoBarber: Página de login

--- 05-gobarber-web/src/styles/global.ts
+++ 05-gobarber-web/src/styles/global.ts
@@ -1,15 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  *{
+  * {
     margin: 0;
     padding: 0;
-    box-sizing: 0;
     outline: 0;
+    box-sizing: border-box;
   }
 
   body {
-    background: #312e38;
+    background: #312E38;
     color: #fff;
     -webkit-font-smoothing: antialiased;
   }
@@ -19,7 +19,7 @@ export default createGlobalStyle`
     font-size: 16px;
   }
 
-  h1, h2, h3, h4, h5, h6 {
+  h1, h2, h3, h4, h5, h6, strong {
     font-weight: 500;
   }
 
