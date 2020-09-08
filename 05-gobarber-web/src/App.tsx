d8e3217aed5a164f99dc64006a5dfd4ceb7f9b78
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2020-09-07 22:29:32 -0300

    GoBarber: API de Contexto

--- 05-gobarber-web/src/App.tsx
+++ 05-gobarber-web/src/App.tsx
@@ -4,11 +4,16 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
+      <AuthContext.Provider value={{ name: 'Jhonatan' }}>
+        <SignIn />
+      </AuthContext.Provider>
+
       <GlobalStyle />
-      <SignIn />
     </>
   );
 };
