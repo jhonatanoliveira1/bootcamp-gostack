d629ef23c56258d8b64ce40bd147d15f16344261
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-02-10 23:07:52 -0300

    Salvando tokens no banco

--- 03-iniciando-back-end/src/shared/infra/http/routes/index.ts
+++ 03-iniciando-back-end/src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRoutes);
 
 export default routes;
