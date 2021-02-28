e8a0f924fd71ffc8e5fc225da658d1b9812044a5
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-02-27 22:58:37 -0300

    Listando dias disponíveis

--- 03-iniciando-back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ 03-iniciando-back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -20,7 +21,7 @@ class ListProviderMonthAvailabilityService {
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
-  public async execute({ provider_id, month, year }: IRequest): Promise<IResponse> {
+  public async execute({ provider_id, year, month }: IRequest): Promise<IResponse> {
     const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
@@ -29,9 +30,27 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      {
+        length: numberOfDaysInMonth,
+      },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
