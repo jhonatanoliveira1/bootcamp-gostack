b5c53a97a79d38654e8111ae166f16bf7e9852d8
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-05-10 22:36:06 -0300

    Criando página de redefinição

--- nivel-05/01-finalizando-frontend-web-do-app/src/pages/ResetPassword/styles.ts
+++ nivel-05/01-finalizando-frontend-web-do-app/src/pages/ResetPassword/styles.ts
@@ -5,7 +5,6 @@ import signInBackground from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
   height: 100vh;
-
   display: flex;
   align-items: stretch;
 `;
@@ -15,7 +14,6 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   width: 100%;
   max-width: 700px;
 `;
@@ -25,7 +23,6 @@ const appearFromLeft = keyframes`
     opacity: 0;
     transform: translateX(-50px);
   }
-
   to {
     opacity: 1;
     transform: translateX(0);
@@ -37,46 +34,37 @@ export const AnimationContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   animation: ${appearFromLeft} 1s;
-
   form {
     margin: 80px 0;
     width: 340px;
     text-align: center;
-
     h1 {
       margin-bottom: 24px;
     }
   }
-
     a {
       color: #f4ede8;
       display: block;
       margin-top: 24px;
       text-decoration: none;
       transition: color 0.2s;
-
       &:hover {
         color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
-
   > a {
     color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
-
     display: flex;
     align-items: center;
-
     svg {
       margin-right: 16px;
     }
-
     &:hover {
       color: ${shade(0.2, '#ff9000')};
     }
