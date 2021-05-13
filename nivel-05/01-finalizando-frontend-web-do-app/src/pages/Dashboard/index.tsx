e4aa7219a3313b7ecc9891f056a54f7ce8bb303f
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-05-12 22:56:34 -0300

    Próximo agendamento

--- nivel-05/01-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
+++ nivel-05/01-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -29,6 +38,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/58116030?v=4"
+                alt="Jhonatan de Oliveira"
+              />
+
+              <strong>Jhonatan de Oliveira</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
