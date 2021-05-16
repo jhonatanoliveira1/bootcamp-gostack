9ed3859a226dd9a4172a485bd0abdade626f862a
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-05-15 22:04:49 -0300

    Agendamentos da API

--- nivel-05/01-finalizando-frontend-web-do-app/src/components/Button/index.tsx
+++ nivel-05/01-finalizando-frontend-web-do-app/src/components/Button/index.tsx
@@ -6,7 +6,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   loading?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, loading,...rest }) => {
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => {
   return (
     <Container type="button" {...rest}>
       {loading ? 'Carregando...' : children}
