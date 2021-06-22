cdf68259a1ad941d5c268adc811f66410d49f564
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-06-21 23:10:32 -0300

    Header do Dashboard

---
+++ nivel-05/02-finalizando-front-end-mobile-do-app/src/pages/Dashboard/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.view`
+  flex: 1;
+`;
+
+export const Header = styled.view`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.text`
+  color: #f4efe8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Regular';
+  line-height: 8px;
+`;
+
+export const UserName = styled.text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+`;
