cfe876ef071b28eb9032e5b1f0ec147ec6595ff7
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-10-02 23:25:31 -0300

    Desafios

---
+++ challenges/desafio-fundamentos-nodejs/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,57 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          return {
+            income: accumulator.income + transaction.value,
+            outcome: accumulator.outcome,
+          };
+        }
+        return {
+          income: accumulator.income,
+          outcome: accumulator.outcome + transaction.value,
+        };
+      },
+      { income: 0, outcome: 0 },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
